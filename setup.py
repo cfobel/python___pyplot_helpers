aa4c284f5ada617a91912ddbd688ba07d0015908
Christian Fobel <christian@fobel.net>
2012-06-28 16:13:41 -0400

    Add setup.py script to allow installation

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "pyplot-helper",
+    version = "0.01",
+    description = "A helper package for matplotib/pyplot.",
+    keywords = "pyplot matplotib",
+    author = "Christian Fobel",
+    url = "https://github.com/cfobel/python___pyplot_helper",
+    license = "LGPL",
+    long_description = """""",
+    packages = ['pyplot_helper']
+)
