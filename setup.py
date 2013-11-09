cb16213f982e5bacbc160c78d04026b8a8e58341
Christian Fobel <christian@fobel.net>
2013-11-09 11:58:34 -0500

    Rename package to pyplot_helpers

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 from distutils.core import setup
 
-setup(name = "pyplot-helper",
+setup(name = "pyplot-helpers",
     version = "0.01",
     description = "A helper package for matplotib/pyplot.",
     keywords = "pyplot matplotib",
     author = "Christian Fobel",
-    url = "https://github.com/cfobel/python___pyplot_helper",
+    url = "https://github.com/cfobel/python___pyplot_helpers",
     license = "LGPL",
     long_description = """""",
-    packages = ['pyplot_helper']
+    packages = ['pyplot_helpers']
 )
